fdfe610a24968b05cf69339549eec0d54d1f2ff9
kimze80 <kimze80@gmail.com>
2018-10-25 22:16:08 -0200

    desafio mastertech

--- src/App.js
+++ src/App.js
@@ -1,27 +1,92 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      items: [],
+      isLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://desafio-mastertech.herokuapp.com/")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json
+        });
+      });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    var { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Carregando...</div>;
+    } else {
+      return (
+        <div className="App">
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+
+                <img src={item.picture} />
+              </li>
+            ))}
+          </ul>
+          <header>
+            <div class="logo">
+              <img src="arquivos/netflix.png" alt="Logo" />
+            </div>
+            <nav>
+              <ul>
+                <li>Home</li>
+                <li>Desafio</li>
+                <li>Missão</li>
+              </ul>
+            </nav>
+          </header>
+          <div id="banner">
+            <button>
+              <p>Teste por 30 Dias</p>
+            </button>
+          </div>
+          <div class="seriesbox">
+            <div id="series">
+              <div class="imagem" />
+              <h4 />
+              <p />
+            </div>
+
+            <div id="series">
+              <div class="imagem" />
+              <h4 />
+              <p />
+            </div>
+
+            <div id="series">
+              <div class="imagem" />
+              <h4 />
+              <p />
+            </div>
+          </div>
+
+          <div class="info">
+            <div>
+              <h5 />
+            </div>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
